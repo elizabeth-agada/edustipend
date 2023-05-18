357478e5cb6e5122d0c97aa77c5a8e2e38a16806
elizabeth-agada <elizadave6@gmail.com>
2023-05-18 17:14:53 +0100

    fix deployment issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Button() {
   return (
-	<div className='p-10 flex justify-center'>
+	  <div className='p-10 flex justify-center'>
     	<button className="bg-yellow2 px-2 py-1 rounded-md text-sm">Create playlist</button>
     </div>
   )
 }
 
 export default Button
+  
